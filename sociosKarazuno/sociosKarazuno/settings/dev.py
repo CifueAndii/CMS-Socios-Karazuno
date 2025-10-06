662878c4e5ba63cc1740ecb42dbe25e058b7591a
CifueAndii <117041628+CifueAndii@users.noreply.github.com>
2025-10-06 12:42:24 -0300

    carpeta principal

---
+++ sociosKarazuno/sociosKarazuno/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-(%dj7ove_pubt)l&_(sawso@ur@rqoc#@x1wusq**z#ph-85(l"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
